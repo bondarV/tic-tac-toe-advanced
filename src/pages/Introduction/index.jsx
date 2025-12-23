f6e58b2ffcdc1ba67cce71c006770b07bcfc1071
Vitalii <bondarfamily123@gmail.com>
2025-12-23 21:31:57 +0200

    ref: refactor Introduction component to use onNavigate prop and update button styles

--- src/pages/Introduction/index.jsx
+++ src/pages/Introduction/index.jsx
@@ -1,14 +1,13 @@
 import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
 import styles from './index.module.css';
+// import {GameSelection} from "./GameSelection.jsx"; // ВИДАЛИТИ
 import {SettingsForm} from "@/components/form/SettingsForm/SettingsForm.jsx";
 import {Button} from "@/components/ui";
-import {useGame} from "@/context/GameContext";
+import {useGameSettings} from "@/hooks/useGameSettings.js";
 
-export function Introduction() {
+export function Introduction({onNavigate}) {
     const [view, setView] = useState('menu');
-    const {settings, saveSettings} = useGame(); // Дані з контексту
-    const navigate = useNavigate();
+    const {settings, saveSettings} = useGameSettings();
 
     const handleSaveSettings = (newSettings) => {
         saveSettings(newSettings);
@@ -21,17 +20,36 @@ export function Introduction() {
                 <div className={styles.menuContainer}>
                     <h1 className={styles.gameTitle}>TIC TAC TOE</h1>
                     <div className={styles.menuButtons}>
-                        <Button label="Грати" onClick={() => navigate('/game')}/>
-                        <Button label="Налаштування" onClick={() => setView('settings')}/>
+                        <Button
+                            label="Грати"
+                            onClick={() => onNavigate('game')}
+                            className={styles.menuBtn}
+                        />
+                        <Button
+                            label="Налаштування"
+                            onClick={() => setView('settings')}
+                            className={styles.menuBtn}
+                        />
                     </div>
                 </div>
             )}
+
             {view === 'settings' && (
                 <div className={styles.contentContainer}>
-                    <SettingsForm initialSettings={settings} onSave={handleSaveSettings}/>
-                    <Button label="Назад" onClick={() => setView('menu')}/>
+                    <h2>Налаштування гри</h2>
+                    <SettingsForm
+                        initialSettings={settings}
+                        onSave={handleSaveSettings}
+                    />
+                    <div style={{marginTop: '1rem'}}>
+                        <Button
+                            label="Назад"
+                            onClick={() => setView('menu')}
+                        />
+                    </div>
                 </div>
             )}
+
         </div>
     );
 }
